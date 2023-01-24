ccfa37b6dcb2cea5f5845d18a7e3b50546cf35dd
apple <apple@apples-MacBook-Pro.local>
2023-01-24 18:36:29 +0500

    add swagger docs and resolve the password hash issue and create user successfully with hash password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { from, Observable, of } from 'rxjs';
 import { User } from 'src/user/model/user.interface';
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -13,8 +13,8 @@ export class AuthService {
     return from(this.jwtService.signAsync({ user }));
   }
 
-  hashPassword(password: string): Observable<string> {
-    return from<string>(bcrypt.hash(password, 12));
+  public hashPassword(password: string): Observable<string> {
+    return from(bcrypt.hash(password, 12));
   }
 
   comparePasswords(
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const swagger = new DocumentBuilder()
+    .setTitle('SVT')
+    .setDescription('Rest API for Blog')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swagger);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
